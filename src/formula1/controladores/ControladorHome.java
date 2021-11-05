70b53f3a04f9acc36fb550435f57b8cf1393b1e8
SebasMejia1 <89583605+SebasMejia1@users.noreply.github.com>
2021-11-05 10:24:13 -0500

    VISTAS Y CONTROLADORES

---
+++ src/formula1/controladores/ControladorHome.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formula1.controladores;
+
+import formula1.modelos.ConsultasEscuderia;
+import formula1.modelos.Escuderia;
+import formula1.modelos.Piloto;
+import formula1.vistas.VistaHome;
+import formula1.vistas.VistaIngreso;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author 503
+ */
+public class ControladorHome implements ActionListener {
+    
+    VistaHome vistahome = new VistaHome();
+    Escuderia escuderia = new Escuderia();
+    Piloto piloto = new Piloto();
+
+    public ControladorHome(VistaHome vistahome,Escuderia escuderia, Piloto piloto ) {
+        
+        this.escuderia = escuderia;
+        this.piloto = piloto;
+        this.vistahome = vistahome;
+        vistahome.BotonAgregar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+        ConsultasEscuderia consultasEscuderia = new ConsultasEscuderia();
+        String id = vistahome.caja1.getText();
+        
+        if(consultasEscuderia.buscarEscuderia(id)!= null){
+        
+    }else{
+        
+            VistaIngreso vistaIngreso = new VistaIngreso();
+            vistahome.setVisible(false);
+            vistaIngreso.setVisible(true);
+    }
+    
+    
+    }
+}
+ 
---
+++ src/formula1/controladores/ControladorIngreso.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formula1.controladores;
+
+import formula1.modelos.ConsultasEscuderia;
+import formula1.modelos.ConsultasPilotos;
+import formula1.modelos.Escuderia;
+import formula1.modelos.Piloto;
+import formula1.vistas.VistaHome;
+import formula1.vistas.VistaIngreso;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author 503
+ */
+public class ControladorIngreso implements ActionListener {
+    
+    VistaIngreso vistaingreso = new VistaIngreso();
+    Escuderia escuderia = new Escuderia();
+    Piloto piloto = new Piloto();
+    
+    public ControladorIngreso(VistaIngreso vistaingreso,Escuderia escuderia, Piloto piloto ) {
+        
+        this.escuderia = escuderia;
+        this.piloto = piloto;
+        this.vistaingreso = vistaingreso;
+        vistaingreso.botonIngresar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+        ConsultasEscuderia consultasEscuderia = new ConsultasEscuderia();
+        
+        ConsultasPilotos consultasPilotos = new ConsultasPilotos();
+        
+        escuderia.setId(vistaingreso.CajaIdEscuderia.getText());
+        escuderia.setNombre(vistaingreso.CajaNombre.getText());
+        escuderia.setPresupuesto(Integer.parseInt(vistaingreso.CajaPresupuesto.getText()));
+        escuderia.setTipoMotor(vistaingreso.CajaMotor.getText());
+        
+        piloto.setNombres(vistaingreso.CajaPiloto.getText());
+        piloto.setApellidos(vistaingreso.CajaApellido.getText());
+        piloto.setSalario(Integer.parseInt(vistaingreso.CajaSalario.getText()));
+        
+        Date entrada = new Date();
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String fechaEntrada = formato.format(entrada);
+    
+    }
+        
+    
+}
